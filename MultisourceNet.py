eeba80f60718feef0a871fab587e7d2a0629e58f
sealpuppy <oldiegoodie99@gmail.com>
2019-01-17 16:48:14 +0800

    try

--- MultisourceNet.py
+++ MultisourceNet.py
@@ -182,19 +182,27 @@ mixloader = torch.utils.data.DataLoader(dataset = mixset,
 class featureNet(nn.Module):
     def __init__(self):
         super(featureNet, self).__init__()
-        self.fc1 = nn.Linear(256*128, 500)
-        self.fc2 = nn.Linear(500, 256)
+        self.conv1 = nn.Conv2d(1, 4, kernel_size=(2,2), stride=2)
+        self.conv2 = nn.Conv2d(4, 8, kernel_size=(2,2), stride=2)
+        self.maxpool = nn.MaxPool2d(kernel_size = (2,2))
+        self.batchnorm = nn.BatchNorm2d(8)
+        self.fc1 = nn.Linear(16*8*8, 512)
+        self.fc2 = nn.Linear(512, 256)
         self.fc3 = nn.Linear(256, 10)
 
         
     def forward(self, x):
-        x = x.view(-1, 256*128)
+        x = x.view(bs, 1 ,256, 128)
+        x = F.relu(self.maxpool(self.conv1(x)))
+        x = F.relu(self.maxpool(self.conv2(x)))
+        x = self.batchnorm(x)
+        x = x.view(-1, 1024)
         x = F.relu(self.fc1(x))
         feat = F.relu(self.fc2(x))
-        x = torch.sigmoid(self.fc3(feat))
+        x = self.fc3(feat)
         
-        return feat, x
-
+        return feat, F.log_softmax(x, dim = 1)
+    
 model = featureNet()
 model.load_state_dict(torch.load('/home/tk/Documents/FeatureNet.pkl'))
     
