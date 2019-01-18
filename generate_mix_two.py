19e99ba2647d9fed21c8933f9ea2b04bd38e82c1
sealpuppy <oldiegoodie99@gmail.com>
2019-01-18 15:38:06 +0800

    2 mix

--- generate_mix_two.py
+++ generate_mix_two.py
@@ -6,33 +6,34 @@ import os
 root_dir = '/home/tk/Documents/'
 sliced_pool_path = '/home/tk/Documents/sliced_pool/'
 
-full_audio = ['birdstudybook', 'captaincook', 'cloudstudies_02_clayden_12', 
-              'constructivebeekeeping',
+female_audio = ['birdstudybook', 'captaincook', 'cloudstudies_02_clayden_12', 
               'discoursesbiologicalgeological_16_huxley_12', 
-              'natureguide', 'pioneersoftheoldsouth', 
-              'pioneerworkalps_02_harper_12', 
-              'romancecommonplace', 'travelstoriesretold']
+              'natureguide', 'pioneerworkalps_02_harper_12']
+
+male_audio = ['constructivebeekeeping', 'pioneersoftheoldsouth',
+                'romancecommonplace','travelstoriesretold']
+
+full_audio = male_audio + female_audio
+
+selection_space = male_audio
 
 mix_spec = []
 spec0_cluster = []
 spec1_cluster = []
 label =[]
 
-
-
 for i in range(10):
     for j in range(100):
 
         # randomly select 2 numbers from 0~9 as index of full_audio
-        ind = np.random.choice([0,1,2,3,4,5,6,7,8,9], 
-            size = 2, replace = False)
+        ind = np.random.choice(len(selection_space), size = 2, replace = False)
 
         # generate spectrograms 
-        spec_file0 = np.random.choice(os.listdir(sliced_pool_path + full_audio[int(ind[0])] + '/for_mix/'))
-        spec0 = gen_spectrogram(sliced_pool_path + full_audio[int(ind[0])] + '/for_mix/' + spec_file0)
+        spec_file0 = np.random.choice(os.listdir(sliced_pool_path + selection_space[int(ind[0])] + '/for_mix/'))
+        spec0 = gen_spectrogram(sliced_pool_path + selection_space[int(ind[0])] + '/for_mix/' + spec_file0)
 
-        spec_file1 = np.random.choice(os.listdir(sliced_pool_path + full_audio[int(ind[1])] + '/for_mix/'))
-        spec1 = gen_spectrogram(sliced_pool_path + full_audio[int(ind[1])] + '/for_mix/' + spec_file1)
+        spec_file1 = np.random.choice(os.listdir(sliced_pool_path + selection_space[int(ind[1])] + '/for_mix/'))
+        spec1 = gen_spectrogram(sliced_pool_path + selection_space[int(ind[1])] + '/for_mix/' + spec_file1)
         print ('spec0 = ', spec_file0, 'spec1 = ', spec_file1)
         # generate mix_spectrograms
         mixed_spec = spec0 + spec1
@@ -46,15 +47,15 @@ for i in range(10):
 
 
     mix_spec = np.array(mix_spec)
-    mix_spec = np.atack(mix_spec)
+    mix_spec = np.stack(mix_spec)
     print ('mixed_spec shape = ', mix_spec.shape)
 
     spec0_cluster = np.array(spec0_cluster)
-    spec0_cluster = np.atack(spec0_cluster)
+    spec0_cluster = np.stack(spec0_cluster)
     print ('target_spec shape = ', spec0_cluster.shape)
 
     label = np.array(label)
-    label = np.atack(label)
+    label = np.stack(label)
     print ('label shape = ', label.shape)
 
     with open(root_dir + "mix_pool/two_mix_spec/mix_spec/mix_spec" + str(i) + '.json', 'w') as jh:
