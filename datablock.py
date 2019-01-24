123facc7bfe996e8d064af7847457dc5eb3b3d2f
sealpuppy <oldiegoodie99@gmail.com>
2019-01-24 21:00:42 +0800

    block

--- datablock.py
+++ datablock.py
@@ -21,11 +21,13 @@ for count in [0,1,2,3,4,5,6,7,8,9]:
 		cleanfolder.append(file_name)
 
 	for j in cleanfolder:
+		print (j)
 		with open(clean_dir + '{}'.format(j)) as f:
 			clean_list.append((json.load(f)))
 
 
 	cleanblock = np.stack(clean_list)
+	print (cleanblock.shape)
 
 	with open(clean_dir + 'datablock' + str(count) + '.json', 'w') as jh:
 	    json.dump(cleanblock.tolist(), jh)
