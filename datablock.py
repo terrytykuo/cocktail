45e7c8471c66cf87c12c36693777ef9cc3c65f04
sealpuppy <oldiegoodie99@gmail.com>
2019-01-24 21:34:46 +0800

    block

--- datablock.py
+++ datablock.py
@@ -23,7 +23,7 @@ for count in [1,2,3,4,5,6,7,8,9]:
 	for i in full_audio:
 		file_name = str(i) + str(count) + '.json'
 		print (file_name)
-		file_list.append((json.load(f)))
+		file_list.append(file_name)
 
 	for j in file_list:
 		print (j)
