1fbe63a935c24bc472a76d64c44d10ea23620c06
sealpuppy <oldiegoodie99@gmail.com>
2019-01-24 20:37:36 +0800

    block

---
+++ datablock.py
@@ -0,0 +1,15 @@
+import numpy as np 
+import json
+
+
+
+clean_dir = root_dir + 'clean/'
+cleanfolder = os.listdir(clean_dir)
+
+for i in cleanfolder:
+	with open(clean_dir + '{}'.format(i)) as f:
+    	clean_list.append(torch.Tensor(json.load(f)))
+
+
+cleanblock = np.stack(clean_list)
+print (cleanblock.shape)
