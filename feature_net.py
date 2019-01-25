4b9eafbdbf1ed59959ffc87bd766c73e39d17c70
LYD <liaoyd16@mails.tsinghua.edu.cn>
2019-01-25 17:58:36 +0800

    training feature_net.py

--- feature_net.py
+++ feature_net.py
@@ -188,30 +188,28 @@ class featureNet(nn.Module):
         )
         self.maxpool7 = nn.MaxPool2d(kernel_size=2)
 
-        self.fc1 = nn.Linear(1024, 512)
-        self.fc2 = nn.Linear(512, 10)
+        self.fc = nn.Linear(1024, 10)
 
     def forward(self, x):
         x = x.view(bs, 1 ,256, 128)
         
-        x = F.relu(self.conv1(x))
+        x = self.conv1(x)
         x = self.maxpool1(x)
-        x = F.relu(self.conv2(x))
+        x = self.conv2(x)
         x = self.maxpool2(x)
-        x = F.relu(self.conv3(x))
+        x = self.conv3(x)
         x = self.maxpool3(x)
-        x = F.relu(self.conv4(x))
+        x = self.conv4(x)
         x = self.maxpool4(x)
-        x = F.relu(self.conv5(x))
+        x = self.conv5(x)
         x = self.maxpool5(x)
-        x = F.relu(self.conv6(x))
+        x = self.conv6(x)
         x = self.maxpool6(x)
-        x = F.relu(self.conv7(x))
+        x = self.conv7(x)
         x = self.maxpool7(x)
 
         x = x.reshape(bs, 1024)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc(x))
 
         return F.log_softmax(x, dim = 1)
     
