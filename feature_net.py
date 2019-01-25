5685ee6e27ddbf08134fb60a6cef4a8a611f3a97
LYD <liaoyd16@mails.tsinghua.edu.cn>
2019-01-25 18:21:49 +0800

    training feature_net.py

--- feature_net.py
+++ feature_net.py
@@ -129,7 +129,7 @@ class ResBlock(nn.Module):
         zeros = torch.zeros( (x.size()[0], channels_out - channels_in, x.shape[2], x.shape[3]), dtype=torch.float )
         return torch.cat((x, zeros), dim=1)
 
-
+'''
 class featureNet(nn.Module):
     def __init__(self):
         super(featureNet, self).__init__()
@@ -214,7 +214,32 @@ class featureNet(nn.Module):
         x = F.relu(self.fc(x))
 
         return F.log_softmax(x, dim = 1)
-    
+'''
+
+class featureNet(nn.Module):
+    def __init__(self):
+        super(featureNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 4, kernel_size=(2,2), stride=2)
+        self.conv2 = nn.Conv2d(4, 8, kernel_size=(2,2), stride=2)
+        self.maxpool = nn.MaxPool2d(kernel_size = (2,2))
+        self.batchnorm = nn.BatchNorm2d(8)
+        self.fc1 = nn.Linear(16*8*8, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 10)
+
+        
+    def forward(self, x):
+        x = x.view(bs, 1 ,256, 128)
+        x = F.relu(self.maxpool(self.conv1(x)))
+        x = F.relu(self.maxpool(self.conv2(x)))
+        x = self.batchnorm(x)
+        x = x.view(-1, 1024)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        
+        return F.log_softmax(x, dim = 1)
+ 
 model = featureNet()
 try:
     model.load_state_dict(torch.load('/home/tk/Documents/FeatureNet.pkl'))
