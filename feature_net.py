c2d4e56f36d8911ff820352d2e4093ce5416720c
LYD <liaoyd16@mails.tsinghua.edu.cn>
2019-01-25 18:07:59 +0800

    training feature_net.py

--- feature_net.py
+++ feature_net.py
@@ -78,8 +78,6 @@ class featureDataSet(Dataset):
             self.spec = torch.Tensor(self.spec[indexes]).squeeze()
             self.labels = torch.Tensor(self.labels[indexes]).squeeze()
 
-            print(self.spec.shape, self.labels.shape)
-
             del indexes
 
         spec = self.spec[offset_in_json]
