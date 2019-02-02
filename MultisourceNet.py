027a011d8a8f4b24b80114ec859950eaf5d7ebdd
LYD <liaoyd16@mails.tsinghua.edu.cn>
2019-02-02 11:16:16 +0800

    train all: fix dataloader

--- MultisourceNet.py
+++ MultisourceNet.py
@@ -103,7 +103,7 @@ def gen_all_pairs():
 all_combinations = gen_all_pairs()
 all_combination_indices = np.arange(CLASSES * (CLASSES-1) // 2)
 
-def gen_rand_pairs(self, num_pairs):
+def gen_rand_pairs(num_pairs):
     ''' 至多C(10,2)对组合 '''
     assert(2 * num_pairs <= CLASSES * (CLASSES - 1))
     ''' 长为 num_pairs 的 list ，为 [0,CLASSES-1]x[0,CLASSES-1] 中的序偶 '''
